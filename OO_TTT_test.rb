3a46f47ae6904a47fd1663c388c214054b11d54d
Brent Palmer <bmpalmer987@gmail.com>
2014-11-13 01:46:48 -0800

    Object Oriented Tic-Tac-Toe game

---
+++ OO_TTT_test.rb
@@ -0,0 +1,31 @@
+#1. Extract classes and objects from existing code
+#2. Write out a discription of the program then extract nouns from discrpition
+#   and grp commmon verbs into nouns
+
+# What is a tic tac toe game?
+
+# A board game with two players. We start with an empty 3x3 board.
+# One player is "X" the other player is "O". The "X" player goes 1st.
+# Two players alternate to mark empty squares. Alternate turns until
+# a player gets 3 squares in a row. If all squares are marked and nobody won
+# then it is a tie.
+
+
+
+
+#game engine
+# draw a baord
+
+# assign player to "X"
+# assign computer to "O"
+
+# loop until winner or all squares are filled
+#    player checks empty square
+#    check for winner
+#    computer picks empty square
+#    check for winner 
+
+# if there is a winner
+#   show the winner
+# or else
+#   it's a tie
